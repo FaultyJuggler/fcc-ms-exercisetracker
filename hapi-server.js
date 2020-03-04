3a4e32f12155ea6be0d2c05b7c463450aa54ccae
felip <ph.bell.cpe@gmail.com>
2020-03-03 22:55:02 -0500

    hapi version working code cleanup

--- hapi-server.js
+++ hapi-server.js
@@ -1,13 +1,71 @@
+// external
 const Hapi = require( '@hapi/hapi' );
 const Mongoose = require( 'mongoose' );
-const Joi = require( '@hapi/joi' );
+const inert = require( '@hapi/inert' );
+const getSecret = require( 'docker-secret' ).getSecret;
+// internal
 
-const server = new Hapi.Server( {
-  host: 'localhost',
-  port: process.env.PORT || 3000,
-} );
+Mongoose.connect(
+    getSecret( 'MONGO_URI' ) || process.env.MONGO_URI,
+    {useNewUrlParser: true, useUnifiedTopology: true} );
 
-server.route( {
-  path: '/api/exercise',
-  handler: '',
-} );
+const startWithPlugins = async function()
+{
+  const server = new Hapi.Server( {
+    host: 'localhost',
+    port: process.env.PORT || 3000,
+  } );
+
+  // server.connection({ routes: { cors: true } })
+
+  console.log( 'server defined' );
+
+  await server.register( inert );
+  // server.register(Vision, function (err) {
+  //   if (err) {
+  //     console.log('Cannot register vision')
+  //   }
+
+  // configure template support
+  //   server.views({
+  //     engines: {
+  //       html: Handlebars
+  //     },
+  //     path: __dirname + '/views',
+  //     layout: 'layout'
+  //   })
+  // })
+  //
+  // console.log( 'views added' )
+
+  server.route( {
+    method: 'GET',
+    path: '/',
+    handler( req, h )
+    {
+      return h.file( __dirname + '/views/index.html' );
+    },
+  } );
+
+  console.log( 'near routes initiated' );
+
+  await server.register(
+      [{plugin: require( './routes/hapi-exercises' )}],
+  );
+
+  console.log( 'plugins registered' );
+
+  try
+  {
+    await server.start();
+  } catch( err )
+  {
+    console.error( err );
+    process.exit( 1 );
+  }
+  console.log( `Server running at: ${ server.info.uri }` );
+};
+
+startWithPlugins();
+
+// exports.server = server;
--- routes/hapi-exercises.js
+++ routes/hapi-exercises.js
@@ -1,94 +1,150 @@
+'use strict';
+
 const Users = require( '../models/user' );
 const Exercises = require( '../models/exercise' );
-const router = require( 'express' ).Router();
 
-module.exports = router;
-
-router.route( '/new-user' ).post( async( req, res ) =>
-{
-  try
+exports.plugin = {
+  pkg: require( '../package.json' ),
+  name: 'exercise_routes',
+  register: async( server, options ) =>
   {
-    const newUser = new Users( req.body );
-    await newUser.save();
-    res.status( 201 ).send( newUser );
-  } catch( e )
-  {
-    res.status( 400 ).send( e );
-  }
-} );
-
-router.route( '/users' ).get( async( req, res ) =>
-{
-  try
-  {
-    const users = await Users.find().select( '-_id -__v' );
-    res.status( 200 ).send( users );
-  } catch( e )
-  {
-    res.status( 400 ).send( e );
-  }
-} );
-
-router.route( '/add' ).post( async( req, res ) =>
-{
-  try
-  {
-    const userExists = await Users.exists( {_id: req.body.userId} );
-    if( userExists )
-    {
-      const newExercise = new Exercises( req.body );
-      if( req.body.date.length < 1 )
+    const basePath = '/api/exercise/';
+    server.route( [
       {
-        newExercise.date = new Date();
-      }
-      await newExercise.save();
-      res.status( 201 ).send( newExercise );
-    } else
-    {
-      throw 'userId does not exist';
-      // res.status(404).send('userId does not exist')
-    }
-  } catch( e )
-  {
-    res.status( 400 ).send( e );
-  }
-} );
+        method: 'POST',
+        path: basePath + 'new-user',
+        handler: async( req, h ) =>
+        {
 
-router.route( '/log' ).get( async( req, res ) =>
-{
-  const userId = req.query.userid;
-  const from = req.query.from || '2019';
-  const to = req.query.to || new Date();
-  const qLimit = req.query.limit || 100;
+          try
+          {
+            const newUser = new Users( req.payload );
+            await newUser.save();
+            return newUser;
+            // const res = h.response( newUser )
+            // res.code( 201 )
+            // res.header('Content-Type', 'application/json')
+            //
+            // return res
+          } catch( e )
+          {
+            console.log( e );
+            return h.response( e ).code( 400 );
+          }
+        },
+      },
+    ] );
 
-  console.log( userId );
+    server.route( [
+      {
+        method: 'GET',
+        path: basePath + 'users',
+        handler: async( req, h ) =>
+        {
 
-  try
-  {
-    const user = ( await Users.findOne( {_id: userId} ) ).toJSON();
+          try
+          {
+            const users = await Users.find().select( '-_id -__v' );
+            return users;
+            // const res = h.response( users )
+            // res.code( 201 )
+            // return res
+          } catch( e )
+          {
+            console.log( e );
+            return h.response( e ).code( 400 );
+          }
+        },
+      },
+    ] );
 
-    if( user )
-    {
-      const exQueryObj = {
-        userId: userId,
-        date: {'$gte': from, '$lte': to},
-      };
+    server.route( [
+      {
+        method: 'POST',
+        path: basePath + 'add',
+        handler: async( req, h ) =>
+        {
 
-      const exercises = await Exercises.find( exQueryObj ).
-          limit( qLimit ).
-          select( '-userId -__v -_id' );
+          try
+          {
+            const userExists = await Users.exists( {_id: req.payload.userId} );
+            if( userExists )
+            {
+              const newExercise = new Exercises( req.payload );
+              if( req.payload.date.length < 1 )
+              {
+                newExercise.date = new Date();
+              }
+              await newExercise.save();
 
-      user.count = exercises.length;
-      user.exercises = exercises;
+              return newExercise;
+              // const res = h.response( newExercise )
+              // res.code( 201 )
+              // res.header('Content-Type', 'application/json')
+              //
+              // return res
+            } else
+            {
+              throw 'userId does not exist';
+              // res.status(404).send('userId does not exist')
+            }
+          } catch( e )
+          {
+            console.log( e );
+            return h.response( e ).code( 400 );
+          }
+        },
+      },
+    ] );
 
-      res.send( user );
-    } else
-    {
-      throw 'userId does not exist';
-      // res.status(404).send('userId does not exist')
-    }
-  } catch( e )
-  {
-    res.status( 400 ).send( e );
-  }
-} );
+    server.route( [
+      {
+        method: 'GET',
+        path: basePath + 'log',
+        handler: async( req, h ) =>
+        {
+
+          try
+          {
+            const userId = req.query.userid;
+            const from = req.query.from || '2019';
+            const to = req.query.to || new Date();
+            const qLimit = req.query.limit || 100;
+
+            const user = ( await Users.findOne( {_id: userId} ) ).toJSON();
+
+            if( user )
+            {
+              const exQueryObj = {
+                userId: userId,
+                date: {'$gte': from, '$lte': to},
+              };
+
+              const exercises = await Exercises.find( exQueryObj ).
+                  limit( qLimit ).
+                  select( '-userId -__v -_id' );
+
+              user.count = exercises.length;
+              user.exercises = exercises;
+
+              return user;
+              // const res = h.response( user )
+              // res.code( 201 )
+              // res.header('Content-Type', 'application/json')
+              // return res
+            } else
+            {
+              const res = h.response( 'user not found' );
+              res.code( 404 );
+              return res;
+            }
+          } catch( e )
+          {
+            console.log( e );
+            return h.response( e ).code( 400 );
+          }
+        },
+      },
+    ] );
+  },
+};
